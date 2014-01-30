3a854fc219d3e2758bbcd7276f789ba53a0d59af
Brandyn A. White <bwhite@dappervision.com>
2014-01-30 03:30:19 +0000

    Cleaned up UI from playground.js
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- app/scripts/playground.js
+++ app/scripts/playground.js
@@ -17,19 +17,16 @@ function createKey(type, success, error) {
     }
 }
 
-function createQR(WSUrl) {
-    createKey("ws", function (x) {glassSecret = x; $('#qr').html(Mustache.render('<div class="col-md-9"><pre>adb shell \"mkdir -p /sdcard/wearscript && echo \'{{url}}\' > /sdcard/wearscript/qr.txt\"</pre></div><img src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl={{url}}&chld=H|4&choe=UTF-8"\>', {url: WSUrl + '/ws/' + x}))}, function () {alert("Could not get ws.  Are you whitelisted on this server?  See the docs for details.")});
+function createQR(WSUrl, success, error) {
+    createKey("ws", function (secret) {success("https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=" + WSUrl + "/ws/" + secret + "&chld=H|4&choe=UTF-8")}, error);
 }
 
-function connectWebsocket(WSUrl) {
+function connectWebsocket(WSUrl, glassConnectedCallback) {
     var url = WSUrl + "/ws";
     var websocket = new ReconnectingWebSocket(url);
     var ws = new WearScriptConnection(websocket, "playground", Math.floor(Math.random() * 100000));
     function subscription_cb() {
-	if (!ws.exists('glass'))
-            $(".scriptel").prop('disabled', true);
-        else
-            $(".scriptel").prop('disabled', false);
+	glassConnectedCallback(ws.exists('glass'));
     }
     websocket.onopen = function () {
 	subscription_cb();
