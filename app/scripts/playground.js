c4a41ae71a09641a247574b814b0efe2f337cac8
Danny Grove <danny@drgrovellc.com>
2014-03-11 20:07:34 -0400

    Code cleanup

--- app/scripts/playground.js
+++ app/scripts/playground.js
@@ -22,71 +22,71 @@ function createQR(WSUrl, success, error) {
 }
 
 function wearScriptConnectionFactory(websocket, glassConnectedCallback) {
-    function onopen(event) {
-        console.log('opened');
-	ws.subscribe('subscriptions', subscription_cb);
-        ws.subscribe('log', log_cb);
-        ws.subscribe('urlopen', urlopen_cb);
-	subscription_cb();
-    }
-    var ws = new WearScriptConnection(websocket, "playground", Math.floor(Math.random() * 100000), onopen);
-    ws.subscribeTestHandler();
-    function subscription_cb() {
-	glassConnectedCallback(ws.exists('glass'));
-        // TODO(brandyn): Only do this once, then provide a button to refresh
-    }
-    function log_cb(channel, message) {
-        console.log(channel + ': ' + message);
-        // TODO(brandyn): Have a notification that a log message was sent
-    }
-    function gist_modify_cb(channel, gists) {
-        HACK_GIST_MODIFIED = gists;
-        console.log('Gist modified');
-    }
-    function gist_get_cb(channel, gist) {
-        window.HACK_GIST = gist;
-        console.log(channel + ': ' + gist);
-    }
-    function urlopen_cb(channel, url) {
-        window.open(url);
-    }
-    return ws;
+  function onopen(event) {
+    console.log('opened');
+    ws.subscribe('subscriptions', subscription_cb);
+    ws.subscribe('log', log_cb);
+    ws.subscribe('urlopen', urlopen_cb);
+    subscription_cb();
+  }
+  var ws = new WearScriptConnection(websocket, "playground", Math.floor(Math.random() * 100000), onopen);
+  ws.subscribeTestHandler();
+  function subscription_cb() {
+    glassConnectedCallback(ws.exists('glass'));
+    // TODO(brandyn): Only do this once, then provide a button to refresh
+  }
+  function log_cb(channel, message) {
+    console.log(channel + ': ' + message);
+    // TODO(brandyn): Have a notification that a log message was sent
+  }
+  function gist_modify_cb(channel, gists) {
+    HACK_GIST_MODIFIED = gists;
+    console.log('Gist modified');
+  }
+  function gist_get_cb(channel, gist) {
+    window.HACK_GIST = gist;
+    console.log(channel + ': ' + gist);
+  }
+  function urlopen_cb(channel, url) {
+    window.open(url);
+  }
+  return ws;
 }
 
 function runScriptOnGlass(ws, script) {
-    ws.publish('glass', 'script', {'glass.html': script});
+  ws.publish('glass', 'script', {'glass.html': script});
 }
 
 function runLambdaOnGlass(ws, script) {
-    ws.publish('glass', 'lambda', script);
+  ws.publish('glass', 'lambda', script);
 }
 
 window.HACK_runEditorScriptOnGlass = function() {
-    runScriptOnGlass(HACK_WS, window.HACK_EDITOR.getSession().getValue());
+  runScriptOnGlass(HACK_WS, window.HACK_EDITOR.getSession().getValue());
 }
 
 
 window.HACK_runLambdaOnGlass = function(line) {
-    runLambdaOnGlass(HACK_WS, line);
+  runLambdaOnGlass(HACK_WS, line);
 }
 
 function gistGet(ws, gistid, callback) {
-    ws.subscribe(ws.channel(ws.groupDevice, 'gistGet'), callback);
-    ws.publish('gist', 'get', ws.channel(ws.groupDevice, 'gistGet'), gistid);
+  ws.subscribe(ws.channel(ws.groupDevice, 'gistGet'), callback);
+  ws.publish('gist', 'get', ws.channel(ws.groupDevice, 'gistGet'), gistid);
 }
 
 function gistModify(ws, gistid, fileName, content, callback) {
-    var c = ws.channel(ws.groupDevice, 'gistModify');
-    ws.subscribe(c, callback);
-    var files = {};
-    files[fileName] = {content: content};
-    ws.publish('gist', 'modify', c, gistid, undefined, files);
+  var c = ws.channel(ws.groupDevice, 'gistModify');
+  ws.subscribe(c, callback);
+  var files = {};
+  files[fileName] = {content: content};
+  ws.publish('gist', 'modify', c, gistid, undefined, files);
 }
 
 function gistCreate(ws, secret, description, fileName, content, callback) {
-    var c = ws.channel(ws.groupDevice, 'gistCreate');
-    ws.subscribe(c, callback);
-    var files = {};
-    files[fileName] = {content: content};
-    ws.publish('gist', 'create', c, secret, description, files);
+  var c = ws.channel(ws.groupDevice, 'gistCreate');
+  ws.subscribe(c, callback);
+  var files = {};
+  files[fileName] = {content: content};
+  ws.publish('gist', 'create', c, secret, description, files);
 }
