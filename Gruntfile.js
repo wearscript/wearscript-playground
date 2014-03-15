591e722c46ca929f3d9e5226756a3f478cc1a55b
lrvick <lance@lrvick.net>
2014-03-14 22:59:27 -0400

    fix example loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
 
     exec: {
         'api-build': 'cd server; GOPATH=~/.go go build && cd ..',
-        'api-serve': 'killall server; ./server/server &'
+        'api-serve': 'killall server; cd server; ./server &'
     },
 
     // The actual grunt server settings
