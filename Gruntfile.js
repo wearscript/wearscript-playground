e4233e8fa383fd82e7f5e66e242ca6df4ccc0630
lrvick <lance@lrvick.net>
2014-03-17 01:42:08 -0400

    grunt build and serve:dist both now do the right thing

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,26 @@ module.exports = function (grunt) {
   }
   var ipAddress = grunt.option('host') || lookupIpAddress;
 
+  var middleware = function (connect, options) {
+      if (!Array.isArray(options.base)) {
+        options.base = [options.base];
+      }
+
+      // Setup the proxy
+      var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+      // Serve static files.
+      options.base.forEach(function(base) {
+          middlewares.push(connect.static(base));
+      });
+
+      // Make directory browse-able.
+      var directory = options.directory || options.base[options.base.length - 1];
+      middlewares.push(connect.directory(directory));
+
+      return middlewares;
+    }
+
   // Define the configuration for all the tasks
   grunt.initConfig({
 
@@ -94,25 +114,7 @@ module.exports = function (grunt) {
             [ '.tmp'
             , '<%= yeoman.app %>'
             ]
-          ,  middleware: function (connect, options) {
-              if (!Array.isArray(options.base)) {
-                options.base = [options.base];
-              }
-
-              // Setup the proxy
-              var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
-
-              // Serve static files.
-              options.base.forEach(function(base) {
-                  middlewares.push(connect.static(base));
-              });
-
-              // Make directory browse-able.
-              var directory = options.directory || options.base[options.base.length - 1];
-              middlewares.push(connect.directory(directory));
-
-              return middlewares;
-            }
+          ,  middleware: middleware
         }
       },
       test: {
@@ -127,7 +129,8 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          base: '<%= yeoman.dist %>'
+          base: '<%= yeoman.dist %>',
+          middleware: middleware
         }
       }
     },
@@ -404,6 +407,20 @@ module.exports = function (grunt) {
     },
 
 
+    'string-replace': {
+      dist: {
+        options: {
+          replacements: [{
+            pattern: '"{{.WSUrl}}"',
+            replacement: '"ws://" + location.host'
+          }]
+        },
+        files: {
+          '<%= yeoman.dist %>/index.html': '<%= yeoman.dist %>/index.html',
+        }
+      }
+    },
+    
 
     // Run some tasks in parallel to speed up the build process
     concurrent: {
@@ -427,6 +444,9 @@ module.exports = function (grunt) {
     if (target === 'dist') {
       return grunt.task.run([
         'build',
+        'string-replace:dist',
+        'exec:api-serve',
+        'configureProxies:server',
         'connect:dist:keepalive'
       ]);
     }
