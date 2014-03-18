ba5087a83c6b71bc4ae1165defd3e2d9916e2047
Brandyn A. White <bwhite@dappervision.com>
2014-03-18 19:45:59 +0000

    First pass as putting images on the canvas too
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- app/scripts/controllers/annotation.js
+++ app/scripts/controllers/annotation.js
@@ -4,11 +4,12 @@ angular.module('wearscriptPlaygroundApp')
   .controller('AnnotationCtrl', function ($scope,Socket) {
       var ws = Socket.ws;
       $scope.images = {}; // [device] = imageb64
+      this.canvas = document.querySelector('#canvas');
+      this.context = this.canvas.getContext('2d');
 
-      this.initCanvas = function ($canvas) {
-          var context = $canvas.getContext('2d');
-          context.fillStyle = '#FFFFFF'; // set canvas background color
-          context.fillRect(0, 0, $canvas.width, $canvas.height);  // now fill the canvas        
+      this.initCanvas = function () {
+          this.context.fillStyle = '#FFFFFF'; // set canvas background color
+          this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);  // now fill the canvas        
 
           var tool = {};
           function ev_canvas (ev) {
@@ -33,22 +34,21 @@ angular.module('wearscriptPlaygroundApp')
               }
           }
 
-          $canvas.addEventListener('mousedown', ev_canvas, false);
-          $canvas.addEventListener('mousemove', ev_canvas, false);
-          $canvas.addEventListener('mouseup', ev_canvas, false);
-          $canvas.addEventListener('touchmove', ev_canvas, false);
-          $canvas.addEventListener('touchstart', ev_canvas, false);
-          $canvas.addEventListener('touchend', ev_canvas, false);
+          this.canvas.addEventListener('mousedown', ev_canvas, false);
+          this.canvas.addEventListener('mousemove', ev_canvas, false);
+          this.canvas.addEventListener('mouseup', ev_canvas, false);
+          this.canvas.addEventListener('touchmove', ev_canvas, false);
+          this.canvas.addEventListener('touchstart', ev_canvas, false);
+          this.canvas.addEventListener('touchend', ev_canvas, false);
           tool.mousemove = function (ev) {
 
           };
           tool.mousedown = function (ev) {
-              var ctx = $canvas.getContext("2d");
-              ctx.beginPath();
-              ctx.arc(ev._x, ev._y, 50, 0, 2 * Math.PI);
-              ctx.strokeStyle = '#0b61a4';
-              ctx.stroke();
-          };
+              this.context.beginPath();
+              this.context.arc(ev._x, ev._y, 50, 0, 2 * Math.PI);
+              this.context.strokeStyle = '#0b61a4';
+              this.context.stroke();
+          }.bind(this);
           tool.mouseup = function (ev) {
 
           };
@@ -63,18 +63,24 @@ angular.module('wearscriptPlaygroundApp')
 
           };
       }
-      this.initCanvas(document.querySelector('#canvas'));
+      this.initCanvas();
 
 
-      function image_cb(channel, time, imageData, numPoints, description) {
-          $scope.imageData = 'data:image/jpeg;base64,' + btoa(imageData);
+      this.image_cb = function (channel, time, imageData, numPoints, description) {
+          var image = new Image(); 
+          image.onload = function (image_id) {
+              this.context.drawImage(image, 0, 0);
+          }.bind(this);
+          image.src = 'data:image/jpeg;base64,' + btoa(imageData);
+          console.log(image.src.length);
+          document.querySelector('#canvas')
           $scope.numPoints = numPoints;
           $scope.description = description;
           $scope.$apply(); // HACK(brandyn): Not sure why we have to do this
-      }
+      }.bind(this);
 
-      ws.subscribe('annotationimage', image_cb);
+      ws.subscribe('image', this.image_cb);
       $scope.$on('$destroy', function cleanup() {
-          ws.unsubscribe('annotationimage');
+          ws.unsubscribe('image');
       });
   });
