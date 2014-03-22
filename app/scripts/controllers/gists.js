3d867be8ef8425fe6cbdd5f32f6b8b1710f75b7c
Danny Grove <danny@drgrovellc.com>
2014-03-22 00:29:22 -0400

    Fixes issue with gist not applying to scope properly

--- app/scripts/controllers/gists.js
+++ app/scripts/controllers/gists.js
@@ -2,7 +2,9 @@
 
 angular.module('wearscriptPlaygroundApp')
   .controller('GistsCtrl', function ($scope, Gist, Socket, $rootScope, $log, $filter) {
-  Gist.list(function(){
+    $scope.gists = []
+    Gist.list(function(){
     $scope.gists = $filter('orderBy')(Gist.gists,'udated_at',true)
+    $scope.$apply()
   })
 });
