c1743d5e86e572e14337b5a7c2d45e5ed3eb5a70
lrvick <lance@lrvick.net>
2014-04-05 22:52:54 -0400

    grunt now fills in {{.GlassBody}} on its own in the same way go server does

--- Gruntfile.js
+++ Gruntfile.js
@@ -416,12 +416,29 @@ module.exports = function (grunt) {
     'string-replace': {
       dev: {
         options: {
-          replacements: [{
-            pattern: '"{{.WSUrl}}"',
-            replacement: '"ws://" + location.host'
-          }]
-        },
+          replacements: [
+            { pattern: '"{{.WSUrl}}"',
+              replacement: '"ws://" + location.host'
+            },
+            { pattern: "'{{.GlassBody}}'",
+              replacement: function (match, p1, offset, string) {
+                var example = 
+                  ('' + grunt.file.read('server/playground_glass.html'))
+                    .replace(/\\/g, '\\\\')
+                    .replace(/\t/g, '\\t')
+                    .replace(/\n/g, '\\n')
+                    .replace(/\u00A0/g, '\\u00A0')
+                    .replace(/&/g, '\\x26')
+                    .replace(/'/g, '\\x27')
+                    .replace(/"/g, '\\x22')
+                    .replace(/</g, '\\x3C')
+                    .replace(/>/g, '\\x3E');
+                return "'"+example+"'"
+              }
+            }
+          ]
         files: {
+        },
           '.tmp/index.html': '<%= yeoman.app %>/index.html',
         }
       },
