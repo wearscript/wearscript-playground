a1c9856371e634792cf48382964de4868f237608
Brandyn A. White <bwhite@dappervision.com>
2014-04-17 22:07:54 +0000

    Fixed QR screen
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- app/scripts/controllers/setup.js
+++ app/scripts/controllers/setup.js
@@ -2,24 +2,28 @@
 
 angular.module('wearscriptPlaygroundApp')
   .controller('SetupCtrl', function ($scope,$http,Profile, $modalInstance) {
-    $http.post('user/key/ws').success(function(wskey){
-        $scope.wsurl = '';
-        $scope.adb = '';
-        $scope.imageHeight = '0px'
 
-        if( window.innerWidth < 400){
-            $scope.imageHeight = '250px';
-        } else {
-            $scope.imageHeight = '500px';
-        }
+      $scope.wsurl = '';
+      $scope.adb = '';
+      $scope.imageHeight = '0px'
+      $scope.qrshow = false;
+      $scope.qr = function () {
+          $http.post('user/key/ws').success(function(wskey){            
+              var wsurl = WSURL + "/ws/" + wskey;
+              $scope.wsurl = wsurl;
+              $scope.adb = 'adb shell \"mkdir -p /sdcard/wearscript && echo \'' + wsurl + '\' > /sdcard/wearscript/qr.txt\"'
+              if( window.innerWidth < 400){
+                  $scope.imageHeight = '250px';
+              } else {
+                  $scope.imageHeight = '500px';
+              }
+              $scope.qrshow = true;
+              $scope.apply();
+          });
+      }
 
-        var wsurl = WSURL + "/ws/" + wskey;
-        $scope.wsurl = wsurl;
-        $scope.adb = 'adb shell \"mkdir -p /sdcard/wearscript && echo \'' + wsurl + '\' > /sdcard/wearscript/qr.txt\"'
-
-        $scope.ok = function () {
-            $modalInstance.dismiss('cancel');
-        };
-    });
+      $scope.ok = function () {
+          $modalInstance.dismiss('cancel');
+      };
   }
 );
